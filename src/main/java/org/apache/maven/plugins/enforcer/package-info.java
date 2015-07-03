84a262dd0463ea3d6926c1b57fece3beeeed5300
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2015-07-03 13:57:45 +0200

    #2: fix javadoc

--- src/main/java/org/apache/maven/plugins/enforcer/package-info.java
+++ src/main/java/org/apache/maven/plugins/enforcer/package-info.java
@@ -1,7 +1,8 @@
 /**
  * All classes in this package are adapter classes with package of the enforcer mojo, so that mapping works without
  * implementation attribute.
- * See <a href="https://maven.apache.org/guides/mini/guide-configuring-plugins
- * .html#Mapping_Complex_Objects">Mapping_Complex_Objects</a>
+ * @see <a href="https://maven.apache.org/guides/mini/guide-configuring-plugins.html#Mapping_Complex_Objects">
+ *     Mapping_Complex_Objects
+ *     </a>
  */
 package org.apache.maven.plugins.enforcer;
